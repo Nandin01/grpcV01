3bffaf27be04299fad60bb32ff32a72e29407386
Leo <lhagvaa1221@gmail.com>
2022-10-22 22:21:30 +0200

    Initial C# changes

---
+++ CSharpConsoleGrpcClient/CSharpConsoleGrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpConsoleGrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
