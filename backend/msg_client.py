32ef1834a77586311d68dd4996eb51168dcc559a
Nandin-erdene <n.ganbat@tue.nl>
2022-10-23 00:23:38 +0200

    fix: Unary

--- backend/msg_client.py
+++ backend/msg_client.py
@@ -3,8 +3,19 @@ import msg_pb2_grpc
 import time
 import grpc
 
+def get_client_stream_requests():
+    while True:
+        name = input("Please enter a name (or nothing to stop chatting): ")
+
+        if name == "":
+            break
+
+        hello_request = msg_pb2.HelloRequest(greeting = "Hello", name = name)
+        yield hello_request
+        time.sleep(1)
+
 def run():
-    with grpc.insecure_channel("localhost") as channel:
+    with grpc.insecure_channel("localhost:50051") as channel:
         stub = msg_pb2_grpc.messengerStub(channel)
         print("1.SayHello - Unary")
         print("2.ServerSaysHello - Server Side Streaming")
@@ -14,7 +25,7 @@ def run():
 
 
         if rpc_call == "1":
-            hello_request = msg_pb2.HelloRequest(greeting = "Hi", name = "Data")
+            hello_request = msg_pb2.HelloRequest(greeting = "Hi", name = "Lkhagvaa")
             hello_reply = stub.SayHello(hello_request)
             print("Say Hello Response Recieved:")
             print(hello_reply)
