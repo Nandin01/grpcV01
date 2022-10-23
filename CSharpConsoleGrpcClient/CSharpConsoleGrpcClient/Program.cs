25242542991322ae2784375a665684afc028df74
Leo <lhagvaa1221@gmail.com>
2022-10-23 16:48:32 +0200

    C# client fix

--- CSharpConsoleGrpcClient/CSharpConsoleGrpcClient/Program.cs
+++ CSharpConsoleGrpcClient/CSharpConsoleGrpcClient/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
 
 namespace CSharpConsoleGrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var connectionString = "http://localhost:50051";
+
+            var input = new HelloRequest();
+            input.Name = "C# Client";
+            input.Greeting = "This is coming from .NET";
+
+            var channelOptions = new GrpcChannelOptions();
+            channelOptions.Credentials = ChannelCredentials.Insecure;
+
+            var channel = GrpcChannel.ForAddress(connectionString, channelOptions);
+            var client = new messenger.messengerClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine("Waiting for the gRPC server");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Server said : " + reply);
         }
     }
 }
